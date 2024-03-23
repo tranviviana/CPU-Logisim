2888c1fab67ee1828ccb3f55fcd96a7233dacc27
cameron-leung <cameronleung@berkeley.edu>
2024-03-22 21:15:59 -0700

    added b 5 custom test file

---
+++ tests/integration-custom/in/i-type-test.s
@@ -0,0 +1,8 @@
+addi ra x0 10 #ra = 0 + 1010
+andi ra x0 2 #ra = 00 & 10
+ori ra x0 3 #ra = 00 | 11
+xori ra ra 2 #ra = 11 ^ 10
+slli ra ra 4 #ra = 01 << 4
+srli ra ra 3 #ra = 10000 >> 2
+srai ra ra 1 #ra = 100 >> 1
+slti ra ra 1
